ac7f4f3525fac123f45af824d1078ad68f4306b0
Rashmi Kanta Behera <beherarashmikanta99@gmail.com>
2026-06-17 14:57:03 +0530

    updated 1

--- src/app/(dashboard)/courses/page.tsx
+++ src/app/(dashboard)/courses/page.tsx
@@ -1,6 +1,8 @@
 import prisma from "@/lib/prisma";
 import { CoursesClient } from "./CoursesClient";
 
+export const dynamic = "force-dynamic";
+
 export default async function CoursesPage() {
   const courses = await prisma.course.findMany({
     select: {
